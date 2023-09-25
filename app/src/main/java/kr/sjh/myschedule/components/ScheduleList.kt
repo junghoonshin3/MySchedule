797cce2618d3286e1cb53ada982fd47d43deb90c
junghoonshin3 <sjh0826kr@gmail.com>
2023-09-25 16:16:00 +0900

    기존 swiptTodismiss 수정
    1. ScheduleItem 레이아웃 변경
    2. dismissState left -> delete
        dismissState right -> complete

--- app/src/main/java/kr/sjh/myschedule/components/ScheduleList.kt
+++ app/src/main/java/kr/sjh/myschedule/components/ScheduleList.kt
@@ -1,40 +1,35 @@
 package kr.sjh.myschedule.components
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.filled.Archive
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Timer
 import androidx.compose.material.icons.filled.TimerOff
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.booleanResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kr.sjh.myschedule.data.local.entity.ScheduleEntity
-import kr.sjh.myschedule.ui.theme.MemoColor
-import kr.sjh.myschedule.ui.theme.TextColor
-import kr.sjh.myschedule.utill.clickableSingle
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun ScheduleList(
@@ -68,132 +63,117 @@ fun ScheduleItem(
     onCompleteSwipe: (ScheduleEntity) -> Unit
 ) {
 
-    val dismissState = rememberDismissState(confirmStateChange = { disMissValue ->
-        when (disMissValue) {
-            DismissValue.Default -> { // dismissThresholds 만족 안한 상태
-                CoroutineScope(Dispatchers.Default).launch {
-                    delay(200)
-                }
-                false
+    var show by remember { mutableStateOf(true) }
+    val dismissState = rememberDismissState(confirmStateChange = {
+        when (it) {
+            DismissValue.DismissedToStart -> {
+                //삭제
+                show = false
+                onDeleteSwipe(schedule)
+
             }
 
-            DismissValue.DismissedToEnd -> { // -> 방향 스와이프 (완료)
-                CoroutineScope(Dispatchers.Default).launch {
-                    delay(200)
-                    onCompleteSwipe(schedule)
-                }
+            DismissValue.DismissedToEnd -> {
+                //완료
+                show = false
+                onCompleteSwipe(schedule)
 
-                true
             }
 
-            DismissValue.DismissedToStart -> { // <- 방향 스와이프 (삭제)
-                CoroutineScope(Dispatchers.Default).launch {
-                    delay(200)
-                    onDeleteSwipe(schedule)
-                }
-                true
-            }
+            DismissValue.Default -> return@rememberDismissState false
         }
+        true
     })
-
-
-    SwipeToDismiss(state = dismissState,
-        dismissThresholds = { FractionalThreshold(0.6f) },
-        background = {
-            val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
-            val color by animateColorAsState(
-                when (dismissState.targetValue) {
-                    DismissValue.Default -> backgroundColor.copy(alpha = 0.5f) // dismissThresholds 만족 안한 상태
-                    DismissValue.DismissedToEnd -> Color.Green.copy(alpha = 0.4f) // -> 방향 스와이프 (완료)
-                    DismissValue.DismissedToStart -> Color.Red.copy(alpha = 0.5f) // <- 방향 스와이프 (삭제)
-                }, label = ""
-            )
-            val icon = when (dismissState.targetValue) {
-                DismissValue.Default -> Icons.Default.Circle
-                DismissValue.DismissedToEnd -> Icons.Default.Done
-                DismissValue.DismissedToStart -> Icons.Default.Delete
-            }
-
-            val scale by animateFloatAsState(
-                when (dismissState.targetValue == DismissValue.Default) {
-                    true -> 0.8f
-                    else -> 1.5f
-                }, label = ""
-            )
-
-            val alignment = when (direction) {
-                DismissDirection.EndToStart -> Alignment.CenterEnd
-                DismissDirection.StartToEnd -> Alignment.CenterStart
-            }
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color)
-                    .padding(horizontal = 30.dp),
-                contentAlignment = alignment
-            ) {
-                Icon(
-                    modifier = Modifier.scale(scale), contentDescription = null, imageVector = icon
-                )
-            }
-        },
-        dismissContent = {
-            Card(modifier = Modifier
-                .fillMaxWidth()
-                .defaultMinSize(minHeight = 100.dp)
-                .background(Color.Transparent)
-                .padding(top = 10.dp, bottom = 10.dp)
-                .clickableSingle {
-                    onScheduleClick(schedule)
-                }) {
-                ConstraintLayout(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(10.dp)
-                ) {
-                    val isAlarm = schedule.isAlarm
-                    val (title, memo, timer, alarmTime) = createRefs()
-                    Text(text = schedule.title,
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.ExtraBold,
-                        color = TextColor,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        modifier = Modifier
-                            .width(280.dp)
-                            .constrainAs(title) {
-                                top.linkTo(parent.top)
-                                start.linkTo(parent.start)
-                            })
-                    Text(text = schedule.memo,
-                        fontSize = 20.sp,
-                        color = TextColor,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis,
-                        modifier = Modifier
-                            .width(280.dp)
-                            .constrainAs(memo) {
-                                top.linkTo(title.bottom, 5.dp)
-                            })
-                    Icon(imageVector = if (isAlarm) Icons.Filled.Timer else Icons.Filled.TimerOff,
-                        contentDescription = "timer",
-                        modifier = Modifier.constrainAs(timer) {
-                            end.linkTo(parent.end)
-                            top.linkTo(title.top)
-                        })
-                    Text(text = if (isAlarm) schedule.alarmTime.toLocalTime().toString() else "",
-                        fontSize = 18.sp,
-                        color = TextColor,
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .constrainAs(alarmTime) {
-                                top.linkTo(timer.bottom, 5.dp)
-                                end.linkTo(timer.end)
-                            })
-
-
-                }
-            }
+    AnimatedVisibility(
+        show, exit = fadeOut(spring())
+    ) {
+        SwipeToDismiss(state = dismissState, background = {
+            DismissBackground(dismissState = dismissState)
+        }, dismissThresholds = { FractionalThreshold(0.7f) }, dismissContent = {
+            ScheduleCard(schedule, onScheduleClick)
         })
+    }
+
+    LaunchedEffect(show) {
+        if (!show) {
+            delay(800)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ScheduleCard(
+    schedule: ScheduleEntity, onScheduleClick: (ScheduleEntity) -> Unit
+) {
+    ListItem(modifier = Modifier
+        .padding(bottom = 10.dp, top = 10.dp)
+        .fillMaxSize()
+        .background(Color.White)
+
+        .clickable { onScheduleClick(schedule) }, icon = {
+        Icon(
+            imageVector = if (schedule.isAlarm) {
+                Icons.Default.Timer
+            } else {
+                Icons.Default.TimerOff
+            }, contentDescription = "Icon"
+        )
+    }, text = {
+        Text(
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            fontSize = TextUnit(17f, TextUnitType.Sp),
+            text = schedule.memo
+        )
+    }, secondaryText = {
+        Text(
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            fontSize = TextUnit(15f, TextUnitType.Sp),
+            text = schedule.regDt.format(
+                DateTimeFormatter.ofPattern("yyyy년MM월dd일")
+            )
+        )
+    }, overlineText = {
+        Text(
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            fontSize = TextUnit(20f, TextUnitType.Sp),
+            text = schedule.title
+        )
+    })
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun DismissBackground(dismissState: DismissState) {
+    val color = when (dismissState.dismissDirection) {
+        DismissDirection.StartToEnd -> Color(0xFFFF1744)
+        DismissDirection.EndToStart -> Color(0xFF1DE9B6)
+        else -> Color.Transparent
+    }
+    val direction = dismissState.dismissDirection
+
+    Row(
+        modifier = Modifier
+            .padding(top = 10.dp, bottom = 10.dp)
+            .fillMaxSize()
+            .background(color),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        if (direction == DismissDirection.StartToEnd) Icon(
+            modifier = Modifier.padding(start = 10.dp),
+            imageVector = Icons.Default.Delete,
+            contentDescription = "delete"
+        )
+        Spacer(modifier = Modifier)
+        if (direction == DismissDirection.EndToStart) Icon(
+            modifier = Modifier.padding(end = 10.dp),
+            // make sure add baseline_archive_24 resource to drawable folder
+            imageVector = Icons.Default.Archive, contentDescription = "Archive"
+        )
+    }
 
 }
